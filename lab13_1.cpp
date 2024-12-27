7a22716d4c0da377639a6a279764595805f15a94
kokoro2580 <hatsakon5806@gmail.com>
2024-12-27 23:40:38 +0700

    edited

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,25 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0.0, sum_sq = 0.0, product = 1.0, harmonic_sum = 0.0;
+    double min_val = A[0], max_val = A[0];
+
+    for (int i = 0; i < N; ++i) {
+        sum += A[i];
+        sum_sq += A[i] * A[i];
+        product *= A[i];
+        harmonic_sum += 1.0 / A[i];
+        
+        if (A[i] < min_val) min_val = A[i];
+        if (A[i] > max_val) max_val = A[i];
+    }
+
+    B[0] = sum / N;
+    B[1] = sqrt((sum_sq / N) - (B[0] * B[0]));
+    B[2] = pow(product, 1.0 / N);
+    B[3] = N / harmonic_sum;
+    B[4] = max_val;
+    B[5] = min_val;
+}
